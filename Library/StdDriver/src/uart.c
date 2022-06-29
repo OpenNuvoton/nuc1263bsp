3a8dceee049dd2b458c6e0e85b5add9b2c9f467c
“ycchen5” <YCCHEN5@nuvoton.com>
2022-06-29 14:38:10 +0800

    Add default case to switch for UART driver.
    Change-Id: I9743e84d06f3aa8d6ad406d638163c067bd53672

--- Library/StdDriver/src/uart.c
+++ Library/StdDriver/src/uart.c
@@ -185,12 +185,15 @@ void UART_Open(UART_T* uart, uint32_t u32baudrate)
         case UART1_BASE:
             u8UartClkSrcSel = (CLK->CLKSEL1 & CLK_CLKSEL1_UART1SEL_Msk) >> CLK_CLKSEL1_UART1SEL_Pos;
             u8UartClkDivNum = (CLK->CLKDIV0 & CLK_CLKDIV0_UART1DIV_Msk) >> CLK_CLKDIV0_UART1DIV_Pos;
-            break;     
+            break;
 
         case UART2_BASE:
             u8UartClkSrcSel = (CLK->CLKSEL1 & CLK_CLKSEL1_UART2SEL_Msk) >> CLK_CLKSEL1_UART2SEL_Pos;
             u8UartClkDivNum = (CLK->CLKDIV0 & CLK_CLKDIV0_UART2DIV_Msk) >> CLK_CLKDIV0_UART2DIV_Pos;
-            break;     
+            break;
+
+        default:
+            return;
     }
 
 
@@ -303,18 +306,21 @@ void UART_SetLine_Config(UART_T* uart, uint32_t u32baudrate, uint32_t u32data_wi
             u8UartClkSrcSel = (CLK->CLKSEL1 & CLK_CLKSEL1_UART1SEL_Msk) >> CLK_CLKSEL1_UART1SEL_Pos;
             u8UartClkDivNum = (CLK->CLKDIV0 & CLK_CLKDIV0_UART1DIV_Msk) >> CLK_CLKDIV0_UART1DIV_Pos;
             break;
-        
+
         case UART2_BASE:
             u8UartClkSrcSel = (CLK->CLKSEL1 & CLK_CLKSEL1_UART2SEL_Msk) >> CLK_CLKSEL1_UART2SEL_Pos;
             u8UartClkDivNum = (CLK->CLKDIV0 & CLK_CLKDIV0_UART2DIV_Msk) >> CLK_CLKDIV0_UART2DIV_Pos;
             break;
+
+        default:
+            return;
     }
 
     /* Get PLL clock frequency if UART clock source selection is PLL/2 */
     if(u8UartClkSrcSel == 1ul)
     {
         au32ClkTbl[u8UartClkSrcSel] = (CLK_GetPLLClockFreq()>>1);
-    }   
+    }
 
     /* Set UART baud rate */
     if(u32baudrate != 0)
@@ -391,6 +397,9 @@ void UART_SelectIrDAMode(UART_T* uart, uint32_t u32Buadrate, uint32_t u32Directi
             u8UartClkSrcSel = (CLK->CLKSEL1 & CLK_CLKSEL1_UART2SEL_Msk) >> CLK_CLKSEL1_UART2SEL_Pos;
             u8UartClkDivNum = (CLK->CLKDIV0 & CLK_CLKDIV0_UART2DIV_Msk) >> CLK_CLKDIV0_UART2DIV_Pos;
             break;
+
+        default:
+            return;
     }
 
     /* Get PLL clock frequency if UART clock source selection is PLL/2 */
@@ -470,7 +479,7 @@ uint32_t UART_Write(UART_T* uart, uint8_t *pu8TxBuf, uint32_t u32WriteBytes)
         {
             u32delayno++;
             if(u32delayno >= 0x40000000)
-                return FALSE;
+                return u32Count;
         }
         uart->DAT = pu8TxBuf[u32Count];    /* Send UART Data from buffer */
     }
